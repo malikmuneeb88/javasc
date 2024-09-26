7f213ed39909829bee9044d7b07d80fbf41441cf
Vyro <mmm906191@gmail.com>
2024-09-26 19:17:41 +0500

    somemore

--- script.js
+++ script.js
@@ -1,57 +1,91 @@
-console.log("Hello World!");
+// console.log("Hello World!");
 
 // ARITHEMATIC OPERATOR
-let a = 7;
-let b = 5;
+// let a = 7;
+// let b = 5;
 
-console.log("a = ", a, "& b = ", b);
-console.log("a + b = ", a + b);
-console.log("a + b = ", a - b);
-console.log("a * b = ", a * b);
-console.log("a / b = ", a / b); 
-console.log("a % b = ", a % b);
-console.log("a ** b =", a ** b);
+// console.log("a = ", a, "& b = ", b);
+// console.log("a + b = ", a + b);
+// console.log("a + b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b); 
+// console.log("a % b = ", a % b);
+// console.log("a ** b =", a ** b);
 
 
 // // UNARY OPERATOR
-a++;
-console.log("a =", a);
+// a++;
+// console.log("a =", a);
 
-b--;
-console.log("b =", b);
+// b--;
+// console.log("b =", b);
 
 
 // // ASSIGNMENT OPERATOR
-a += 5;
-console.log("a =", a);
+// a += 5;
+// console.log("a =", a);
 
-a -= 4;
-console.log("a =", a);
+// a -= 4;
+// console.log("a =", a);
 
-b *= 5;
-console.log("b =", b);
+// b *= 5;
+// console.log("b =", b);
 
-b /= 4;
-console.log("b =", b);
+// b /= 4;
+// console.log("b =", b);
 
-b %= 4;
-console.log("b =", b);
+// b %= 4;
+// console.log("b =", b);
 
-a **= 3;
-console.log("a =", a);
+// a **= 3;
+// console.log("a =", a);
 
 //LOGICAL OPERATORS
 
 // && koi aik bhi condition false hogi tw ye false return kre ga
-let cond1 = a < 6;
-let cond2 = b === 5;
+// let cond1 = a < 6;
+// let cond2 = b === 5;
 
-console.log("cond1 && cond2 = ", cond1 && cond2);
+// console.log("cond1 && cond2 = ", cond1 && cond2);
 
 // || koi aik bhi condition true hogi tw ye true return kre ga
-let condition1 = a < 6;
-let condition2 = b === 5;
+// let cond1 = a < 6;
+// let cond2 = b === 5;
 
-console.log("cond1 || cond2 = ", cond1 || cond2);
+// console.log("cond1 || cond2 = ", cond1 || cond2);
 
 // ! ye true ko false or false ko true bna deta hai
+
+
+
+// CONDITIONAL STATEMENTS
+let age = 18;
+
+if (age >= 18) {
+    console.log("You can vote");
+} else {
+    console.log("You cannot Vote") 
+}
+
+
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+} else if (mode === "purple"){
+    color = "purple";
+} else {
+    color = "white";
+}
+ 
+console.log(color);
+
+ 
+let num = 109;
+
+if (num % 2 === 0) {
+    console.log("even");
+} else {
+    console.log("odd");
+}
