8ccedb1fbfaab015ab2f7a41a85b18abc0db414e
Vyro <mmm906191@gmail.com>
2024-09-26 17:41:45 +0500

    coding

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ console.log("a % b = ", a % b);
 console.log("a ** b =", a ** b);
 
 
-// UNARY OPERATOR
+// // UNARY OPERATOR
 a++;
 console.log("a =", a);
 
@@ -21,7 +21,7 @@ b--;
 console.log("b =", b);
 
 
-// ASSIGNMENT OPERATOR
+// // ASSIGNMENT OPERATOR
 a += 5;
 console.log("a =", a);
 
@@ -39,3 +39,19 @@ console.log("b =", b);
 
 a **= 3;
 console.log("a =", a);
+
+//LOGICAL OPERATORS
+
+// && koi aik bhi condition false hogi tw ye false return kre ga
+let cond1 = a < 6;
+let cond2 = b === 5;
+
+console.log("cond1 && cond2 = ", cond1 && cond2);
+
+// || koi aik bhi condition true hogi tw ye true return kre ga
+let condition1 = a < 6;
+let condition2 = b === 5;
+
+console.log("cond1 || cond2 = ", cond1 || cond2);
+
+// ! ye true ko false or false ko true bna deta hai
