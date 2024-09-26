d7bf9d3fe0c75eb5d64f8206ebe74d75f210d627
Vyro <mmm906191@gmail.com>
2024-09-26 17:18:54 +0500

    code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// console.log("Hello World!");
+console.log("Hello World!");
 
 // ARITHEMATIC OPERATOR
 let a = 7;
@@ -12,9 +12,30 @@ console.log("a / b = ", a / b);
 console.log("a % b = ", a % b);
 console.log("a ** b =", a ** b);
 
+
 // UNARY OPERATOR
 a++;
 console.log("a =", a);
 
 b--;
 console.log("b =", b);
+
+
+// ASSIGNMENT OPERATOR
+a += 5;
+console.log("a =", a);
+
+a -= 4;
+console.log("a =", a);
+
+b *= 5;
+console.log("b =", b);
+
+b /= 4;
+console.log("b =", b);
+
+b %= 4;
+console.log("b =", b);
+
+a **= 3;
+console.log("a =", a);
