2dfcfec635e42b2c5f2a9fcf6878dafc1f1685ab
Vyro <mmm906191@gmail.com>
2024-09-28 20:31:05 +0500

    changes

--- loops.js
+++ loops.js
@@ -1,3 +1,13 @@
-for (let i = 1; i <= 10; i++) {
-    console.log("apna college");
+// for (let i = 1; i <= 10; i++) {
+//     console.log("apna college");
+// }
+
+
+// Calculate the sum of 1 to 5
+
+let sum = 0;
+let n = 100;
+for (let i = 1; i <= n; i++) { 
+    sum += i;
 }
+console.log("sum of 5 is", sum);
