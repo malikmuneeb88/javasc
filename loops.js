2408d7244e83e59784c78d31082b496b4688ea08
Vyro <mmm906191@gmail.com>
2024-09-28 20:49:43 +0500

    morecode

--- loops.js
+++ loops.js
@@ -5,9 +5,35 @@
 
 // Calculate the sum of 1 to 5
 
-let sum = 0;
-let n = 100;
-for (let i = 1; i <= n; i++) { 
-    sum += i;
-}
-console.log("sum of 5 is", sum);
+// let sum = 0;
+// let n = 100;
+// for (let i = 1; i <= n; i++) { 
+//     sum += i;
+// }
+// console.log("sum of 5 is", sum);
+
+
+// For-of loop
+
+// let str = "JavaScript"
+// let size = 0;
+
+// for (let i of str) {
+//     console.log(i);
+//     size++;
+// }
+// console.log(size);
+
+
+// For in loop
+
+// let student = {
+//     name: "Malik Muneeb",
+//     age: 22,
+//     cgpa: 2.5,
+//     ispass: true,
+// };
+
+// for (let key in student) {
+//     console.log(key,":", student[key]);
+// }
