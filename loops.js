2cdfbaaf5a33c1a049002b02ce4c1df0f6b04088
Vyro <mmm906191@gmail.com>
2024-09-28 23:47:12 +0500

    Question

--- loops.js
+++ loops.js
@@ -1,6 +1,8 @@
-// for (let i = 1; i <= 10; i++) {
-//     console.log("apna college");
-// }
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
 
 
 // Calculate the sum of 1 to 5
