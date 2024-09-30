b46ac6a9e6b3b7574b63c0fbfb9cd18f8550c237
Vyro <mmm906191@gmail.com>
2024-09-30 10:58:46 +0500

    functionsQuestions

--- functions.js
+++ functions.js
@@ -14,19 +14,44 @@
 // console.log(val);
 
 
-function sum(a, b) {
-    return a + b;
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// // Arrow Function: 
+// const arrowSum = (a, b) => {
+//     console.log(a + b);
+// };
+
+// function mul(a, b) {
+//     return a * b;
+// }
+
+// const arrowMul = (a, b) => {
+//     console.log(a * b);
+// }; 
+
+
+
+// Questions:
+function countVowels(str) {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+    return count;
 }
 
-// Arrow Function: 
-const arrowSum = (a, b) => {
-    console.log(a + b);
-};
 
-function mul(a, b) {
-    return a * b;
+
+const countVow = (str) => {
+    let count = 0;
+    for (const char of str) {
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+    return count;
 }
-
-const arrowMul = (a, b) => {
-    console.log(a * b);
-}; 
