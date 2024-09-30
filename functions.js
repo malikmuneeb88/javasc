7530f3f468ce4e15160c227f945f8bf763171a9d
Vyro <mmm906191@gmail.com>
2024-09-30 10:39:09 +0500

    functions

---
+++ functions.js
@@ -0,0 +1,32 @@
+// function myFunction() {
+//     console.log("This office is beautifull!")
+// }
+
+// myFunction();
+
+
+// function sum (x, y) {
+//     add = x + y;
+//     return add;
+// }
+
+// let val = sum(56, 83);
+// console.log(val);
+
+
+function sum(a, b) {
+    return a + b;
+}
+
+// Arrow Function: 
+const arrowSum = (a, b) => {
+    console.log(a + b);
+};
+
+function mul(a, b) {
+    return a * b;
+}
+
+const arrowMul = (a, b) => {
+    console.log(a * b);
+}; 
--- script.js
+++ script.js
@@ -335,8 +335,8 @@
 
 
 
-let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
-companies.shift();
-companies.splice(2, 1, "Ola");
-companies.push("Amazon");
-console.log(companies);
+// let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+// companies.shift();
+// companies.splice(2, 1, "Ola");
+// companies.push("Amazon");
+// console.log(companies); 
