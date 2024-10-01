3fe281fa513d67d6b694bed8e613cb7c9c438a9e
Vyro <mmm906191@gmail.com>
2024-10-01 19:37:06 +0500

    DOMs

--- DocumentObjectModel.js
+++ DocumentObjectModel.js
@@ -1,1 +1,16 @@
-console.log("hello!");
+// let button = document.getElementById("myId");
+// console.dir(button);
+
+// let paragraphs = document.getElementsByTagName("p");
+// console.dir(paragraphs);
+
+// let headings = document.getElementsByClassName("heading-class");
+// console.dir(headings);
+// console.log(headings);
+
+
+let firstElement = document.querySelector("p");
+console.dir(firstElement);
+
+let allElements = document.querySelectorAll("p");
+console.dir(allElements);
