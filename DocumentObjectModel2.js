d36b2369976e463e60a8496c350412bc32200d1a
Vyro <mmm906191@gmail.com>
2024-10-02 15:45:38 +0500

    DOM2

---
+++ DocumentObjectModel2.js
@@ -0,0 +1,22 @@
+let div = document.querySelector("div");
+// console.log(div);
+
+// let id = div.getAttribute("id");
+// console.log(id);
+
+// let name = div.getAttribute("name");
+// console.log(name);
+
+
+// let para = document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class", "newclass"));
+
+
+div.style.backgroundColor = "red";
+
+div.style.fontSize = "30px";
+
+div.innerText = "My name is malik muneeb";
