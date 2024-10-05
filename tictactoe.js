2fd5a2fa25202ea35b304c7835c50b44956889fb
Vyro <mmm906191@gmail.com>
2024-10-06 02:09:50 +0500

    tictactoe

--- tictactoe.js
+++ tictactoe.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true;
 
@@ -13,6 +16,8 @@ let turnO = true;
     [3, 4, 5],
     [6, 7, 8],
 ];
+
+
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
@@ -30,9 +35,23 @@ boxes.forEach((box) => {
     });
 });
 
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+}
 const checkWinner = () => {
     for(let pattern of winPatterns) {
-        console.log(pattern[0], pattern[1], pattern[2]);
-        console.log(boxes[pattern[0]].innerText, boxes[pattern[1]].innerText, boxes[pattern[2]].innerText);
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log("Winner!", pos1Val);
+                showWinner(pos1Val);
+            }
+        }
     }
 };     
+
+
