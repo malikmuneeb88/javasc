948c73c3d8506ce882aa93959656e7f94167226d
Vyro <mmm906191@gmail.com>
2024-10-10 01:27:11 +0500

    classes and obejcts

--- ClassesandObjects.js
+++ ClassesandObjects.js
@@ -8,5 +8,25 @@ const aqibMuneeb = {
     salary: 50000,
 };
 
+const aqibMuneeb2 = {
+    salary: 50000,
+};
+
+const aqibMuneeb3 = {
+    salary: 50000,
+};
+
+const aqibMuneeb4 = {
+    salary: 50000,
+};
+
+const aqibMuneeb5 = {
+    salary: 50000,
+};
+
 // agr humne first function ko dusre mn call krna ho
+aqibMuneeb.__proto__ = employee;
+aqibMuneeb.__proto__ = employee;
+aqibMuneeb.__proto__ = employee;
+aqibMuneeb.__proto__ = employee;
 aqibMuneeb.__proto__ = employee;
