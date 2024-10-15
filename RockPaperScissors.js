c037097d247f307cece299e8d0b2da736564e3d6
Vyro <mmm906191@gmail.com>
2024-10-15 16:52:41 +0500

    Rock Paper and Scissors

---
+++ RockPaperScissors.js
@@ -0,0 +1,15 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+const playGame = (userChoice) => {
+    console.log(userChoice)
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("#id");
+        playGamea(userChoice)
+    });
+});
