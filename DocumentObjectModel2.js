34df56e654076ff4953f3b601549368c4d22d492
Vyro <mmm906191@gmail.com>
2024-10-17 13:40:34 +0500

    DOM

--- DocumentObjectModel2.js
+++ DocumentObjectModel2.js
@@ -1,51 +1,51 @@
-// let div = document.querySelector("div");
-// console.log(div);
+let div = document.querySelector("div");
+console.log(div);
 
-// let id = div.getAttribute("id");
-// console.log(id);
+let id = div.getAttribute("id");
+console.log(id);
 
-// let name = div.getAttribute("name");
-// console.log(name);
-
-
-// let para = document.querySelector("p");
-// console.log(para.getAttribute("class"));
-
-// let para = document.querySelector("p");
-// console.log(para.setAttribute("class", "newclass"));
+let name = div.getAttribute("name");
+console.log(name);
 
 
-// div.style.backgroundColor = "red";
+let para1 = document.querySelector("p");
+console.log(para1.getAttribute("class"));
 
-// div.style.fontSize = "30px";
-
-// div.innerText = "My name is malik muneeb";
-
-// let newBtn = document.createElement("button");
-// newBtn.innerText = "click me!";
-// console.log(newBtn);
-
-// let div = document.querySelector("div");
-// div.append(newBtn);
-
-// let div = document.querySelector("div");
-// div.prepend(newBtn);
-
-// let div = document.querySelector("div");
-// div.after(newBtn);
-
-// let div = document.querySelector("div");
-// div.before(newBtn);
+let para2 = document.querySelector("p");
+console.log(para2.setAttribute("class", "newclass"));
 
 
+div.style.backgroundColor = "red";
 
-// let newHeading = document.createElement("h1");
-// newHeading.innerHTML = "<i>Hi, I am new !</i>";
+div.style.fontSize = "30px";
 
-// document.querySelector("body").prepend(newHeading);
+div.innerText = "My name is malik muneeb";
 
-// let para = document.querySelector("p");
-// para.remove();
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!";
+console.log(newBtn);
+
+let div = document.querySelector("div");
+div.append(newBtn);
+
+let div = document.querySelector("div");
+div.prepend(newBtn);
+
+let div = document.querySelector("div");
+div.after(newBtn);
+
+let div = document.querySelector("div");
+div.before(newBtn);
+
+
+
+let newHeading = document.createElement("h1");
+newHeading.innerHTML = "<i>Hi, I am new !</i>";
+
+document.querySelector("body").prepend(newHeading);
+
+let para3 = document.querySelector("p");
+para3.remove();
 
 let newBtn = document.createElement("button");
 newBtn.innerText = "click me!";
