2e11e33bd1832fc695cd7da5a77fbb29dee65537
Vyro <mmm906191@gmail.com>
2024-10-17 13:04:23 +0500

    promisses

--- callbacks.js
+++ callbacks.js
@@ -40,46 +40,54 @@
 
 
 //General programming mn data kesay use hotay hain
-// function getData(dataId, getNextData) {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             console.log("data", dataId);
-//             resolve("success");
-//             if(getNextData) {
-//                 getNextData();
-//             }
-//         }, 5000);
-//     })
-// }
+function getData(dataId, getNextData) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("data", dataId);
+            resolve("success");
+        }, 2000);
+    })
+}
+
+//Promise chain
+getData(1)
+    .then((res) => {
+        return getData(2);
+})
+    .then((res) => {
+        return getData(3);
+})
+    .then((res) => {
+        console.log(res);
+    })
+
 
 
 
 //ASYNCHRONOUS FUNCTION
-function asyncFunc1() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("data1");
-            resolve("success");
-        }, 4000);
-    });
-}
+// function asyncFunc1() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data1");
+//             resolve("success");
+//         }, 4000);
+//     });
+// }
 
-function asyncFunc2() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("data2");
-            resolve("success");
-        }, 4000);
-    });
-}
+// function asyncFunc2() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data2");
+//             resolve("success");
+//         }, 4000);
+//     });
+// }
 
-console.log("fetching data1......");
-let p1 = asyncFunc1();
-p1.then((res) => {
-    console.log("fetching data2.......");
-    let p2 = asyncFunc2();
-    p2.then((res) => {
+// console.log("fetching data1......");
+// asyncFunc1().then((res) => {
+//     console.log("fetching data2.......");
+// asyncFunc2().then((res) => {
         
-    });
-});
+//     });
+// });
 
