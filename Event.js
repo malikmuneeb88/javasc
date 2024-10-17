42dbaca91d254012dd13456d6d2849f465114677
Vyro <mmm906191@gmail.com>
2024-10-17 13:38:56 +0500

    Events

--- Event.js
+++ Event.js
@@ -1,47 +1,47 @@
-// let btn1 = document.querySelector("#btn1");
+let btn1 = document.querySelector("#btn1");
 
-// btn1.onclick = (event) => {
-//     console.log(event);   //event object ki value print hogii
-//     console.log(event.type);  //type print hogii
-//     console.log(event.target);  //html mn jo button hai wo aajae gaa
-//     console.log(event.clientX, event.clientY);  
-//     // console.log("btn1 was clicked");
-//     // let a = 25;
-//     // a++;
-//     // console.log(a);
-// }
-
-
-// let box = document.querySelector("div");
-// box.onmouseover = () => {
-//     console.log(event);   //event object ki value print hogii
-//     console.log(event.type);  //type print hogii
-//     console.log(event.target);  //html mn jo button hai wo aajae gaa
-//     console.log(event.clientX, evt.clientY); 
-//     // console.log("you are inside div");
-// }
+btn1.onclick = (event) => {
+    console.log(event);   //event object ki value print hogii
+    console.log(event.type);  //type print hogii
+    console.log(event.target);  //html mn jo button hai wo aajae gaa
+    console.log(event.clientX, event.clientY);  
+    // console.log("btn1 was clicked");
+    // let a = 25;
+    // a++;
+    // console.log(a);
+}
 
 
-// btn1.addEventListener("click", (evt) => {
-//     console.log("button1 was clicked - handler1");
-// });
+let box = document.querySelector("div");
+box.onmouseover = () => {
+    console.log(event);   //event object ki value print hogii
+    console.log(event.type);  //type print hogii
+    console.log(event.target);  //html mn jo button hai wo aajae gaa
+    console.log(event.clientX, evt.clientY); 
+    // console.log("you are inside div");
+}
 
 
-// btn1.addEventListener("click", () => {
-//     console.log("button1 was clicked - handler2");
-// });
+btn1.addEventListener("click", (evt) => {
+    console.log("button1 was clicked - handler1");
+});
 
-// const handler3 = () => {
-//     console.log("button1 was clicked - handler3");
-// }
 
-// btn1.addEventListener("click", handler3);
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - handler2");
+});
 
-// btn1.addEventListener("click", () => {
-//     console.log("button1 was clicked - handler4");
-// });
+const handler3 = () => {
+    console.log("button1 was clicked - handler3");
+}
 
-// btn1.removeEventListener("click", handler3);
+btn1.addEventListener("click", handler3);
+
+btn1.addEventListener("click", () => {
+    console.log("button1 was clicked - handler4");
+});
+
+btn1.removeEventListener("click", handler3);
 
 
 
