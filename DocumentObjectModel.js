afd212d2f9da4cc13b6604f074585f0837549ccd
Vyro <mmm906191@gmail.com>
2024-10-17 13:41:09 +0500

    DOM1

--- DocumentObjectModel.js
+++ DocumentObjectModel.js
@@ -1,19 +1,19 @@
-// let button = document.getElementById("myId");
-// console.dir(button);
+let button = document.getElementById("myId");
+console.dir(button);
 
-// let paragraphs = document.getElementsByTagName("p");
-// console.dir(paragraphs);
+let paragraphs = document.getElementsByTagName("p");
+console.dir(paragraphs);
 
-// let headings = document.getElementsByClassName("heading-class");
-// console.dir(headings);
-// console.log(headings);
+let headings = document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);
 
 
-// let firstElement = document.querySelector("p");
-// console.dir(firstElement);
+let firstElement = document.querySelector("p");
+console.dir(firstElement);
 
-// let allElements = document.querySelectorAll("p");
-// console.dir(allElements);
+let allElements = document.querySelectorAll("p");
+console.dir(allElements);
 
 
 let h2 = document.querySelector("h2");
