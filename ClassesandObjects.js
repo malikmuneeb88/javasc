1266b5ab31364f4b9422d6197ec94e1b83b65f5a
Vyro <mmm906191@gmail.com>
2024-10-17 13:42:10 +0500

    Classes and Objects

--- ClassesandObjects.js
+++ ClassesandObjects.js
@@ -1,104 +1,104 @@
-// const employee = {
-//     calculateTax() {
-//         console.log("tax rate in Pakistan is 50%");
-//     }
-// };
+const employee = {
+    calculateTax() {
+        console.log("tax rate in Pakistan is 50%");
+    }
+};
 
-// const aqibMuneeb = {
-//     salary: 50000,
-//     calculateTax() {
-//         console.log("Tax rate is 20%"); 
-//     }
-// };
+const aqibMuneeb = {
+    salary: 50000,
+    calculateTax() {
+        console.log("Tax rate is 20%"); 
+    }
+};
 
-// const aqibMuneeb2 = {
-//     salary: 50000,
-// };
+const aqibMuneeb2 = {
+    salary: 50000,
+};
 
-// const aqibMuneeb3 = {
-//     salary: 50000,
-// };
+const aqibMuneeb3 = {
+    salary: 50000,
+};
 
-// const aqibMuneeb4 = {
-//     salary: 50000,
-// };
+const aqibMuneeb4 = {
+    salary: 50000,
+};
 
-// const aqibMuneeb5 = {
-//     salary: 50000,
-// };
+const aqibMuneeb5 = {
+    salary: 50000,
+};
 
-// // agr humne first function ko dusre mn call krna ho
-// aqibMuneeb.__proto__ = employee;
-// aqibMuneeb2.__proto__ = employee;
-// aqibMuneeb3.__proto__ = employee;
-// aqibMuneeb4.__proto__ = employee;
-// aqibMuneeb5.__proto__ = employee;
-
-
-
-// class ToyotaCar {
-//     constructor(brand, mileage) {
-//         console.log("creating new object");
-//         this.brand = brand;
-//         this.mileage = mileage;
-//     }
-//     start() {
-//         console.log("start");
-//     }
-
-//     stop() {
-//         console.log("stop");
-//     }
-
-//     // setBrand(brand) {
-//     //     this.brand = brand;
-//     // }
-// }
-
-// let fortuner = new ToyotaCar("fortuner", 10);
-// console.log(fortuner);
-// // fortuner.setBrand("fortuner");
+// agr humne first function ko dusre mn call krna ho
+aqibMuneeb.__proto__ = employee;
+aqibMuneeb2.__proto__ = employee;
+aqibMuneeb3.__proto__ = employee;
+aqibMuneeb4.__proto__ = employee;
+aqibMuneeb5.__proto__ = employee;
 
 
-// let lexus = new ToyotaCar("lexus", 12);
-// console.log(lexus);
-// // lexus.setBrand("lexus");
+
+class ToyotaCar {
+    constructor(brand, mileage) {
+        console.log("creating new object");
+        this.brand = brand;
+        this.mileage = mileage;
+    }
+    start() {
+        console.log("start");
+    }
+
+    stop() {
+        console.log("stop");
+    }
+
+    // setBrand(brand) {
+    //     this.brand = brand;
+    // }
+}
+
+let fortuner = new ToyotaCar("fortuner", 10);
+console.log(fortuner);
+// fortuner.setBrand("fortuner");
 
 
-// class Person {
-//     constructor(name) {
-//         this.species = "homo sapiens";
-//         this.name = name;
-//     }
+let lexus = new ToyotaCar("lexus", 12);
+console.log(lexus);
+// lexus.setBrand("lexus");
 
-//     eat() {
-//         console.log("eat");
-//     }
 
-//     sleep() {
-//         console.log("sleep"); 
-//     }
+class Person {
+    constructor(name) {
+        this.species = "homo sapiens";
+        this.name = name;
+    }
 
-//     work() {
-//         console.log("do nothing");
-//     }
-// }
+    eat() {
+        console.log("eat");
+    }
 
-// class Engineer extends Person {
-//     constructor(name) {
-//         super(name); // To invoke the parent constructor
-//     }
-//     work() {
-//         super.eat();
-//         console.log("solve Problems, build something");
-//     }
-// }
+    sleep() {
+        console.log("sleep"); 
+    }
 
-// let muneebObj = new Engineer("Muneeb");
+    work() {
+        console.log("do nothing");
+    }
+}
 
-// class Child extends Parent {}
+class Engineer extends Person {
+    constructor(name) {
+        super(name); // To invoke the parent constructor
+    }
+    work() {
+        super.eat();
+        console.log("solve Problems, build something");
+    }
+}
 
-// let obj = new Child();
+let muneebObj = new Engineer("Muneeb");
+
+class Child extends Parent {}
+
+let obj = new Child();
 
 
 
